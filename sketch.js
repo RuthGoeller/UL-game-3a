9d6672c8f216db3a125adfbcc189c87eda975ce7
RuthGoeller <jhgoeller@gmail.com>
2023-07-10 23:39:26 -0500

    update

--- sketch.js
+++ sketch.js
@@ -8,11 +8,7 @@ Game interaction
 
 */
 
-/*
- In `setup`, declare and initialise a `clouds` variable with an array containing some cloud objects (e.g. at least 3).         
-
-HINT: you can copy the one from part 2b but vary the x and y positions of each object.*/
-
+var cameraPosX = 0;
 var gameChar_x;
 var gameChar_y;
 var floorPos_y;
@@ -26,12 +22,7 @@ var trees_x;
 var clouds;
 var mountains;
 
-
-
-
-
-function setup()
-{
+function setup() {
 	createCanvas(1024, 576);
 	floorPos_y = height * 3/4;
 	gameChar_x = width/2;
@@ -43,57 +34,67 @@ function setup()
 	isPlummeting = false;
 
 	collectable = {
-		x_pos: 100,
+		x_pos: 70,
 		y_pos: floorPos_y,
 		size: 40,
 		isFound: false
 	};
 
-	trees_x = [100, 300, 500, 700, 900];
+	trees_x =[ 150, 350, 550, 750, 950];
 
-	clouds = [ {x_pos: 100, y_pos: 100}, {x_pos: 300, y_pos: 100}, {x_pos: 500, y_pos: 100} ]; 
+	clouds = [
+		{x_pos: 100, y_pos: 100},
+		{x_pos: 300, y_pos: 100},
+		{x_pos: 500, y_pos: 100}
+	];
 
-	mountains = [ {x_pos: 100, y_pos: 100}, {x_pos: 300, y_pos: 100}, {x_pos: 500, y_pos: 100} ];
+	mountains = [
+		{x_pos: 100, y_pos: 100},
+		{x_pos: 300, y_pos: 100},
+		{x_pos: 500, y_pos: 100}
+	];
+
 	canyon = {
 		x_pos: 0,
 		width: 100
 	};
 }
 
-function draw()
-{
-
+function draw() {
 	///////////DRAWING CODE//////////
 
-	background(100,155,255); //fill the sky blue
-
+	background(100, 155, 255); //fill the sky blue
 
 	noStroke();
-	fill(0,155,0);
+	fill(0, 155, 0);
 	rect(0, floorPos_y, width, height - floorPos_y); //draw some green ground
 
+	// Move the camera
+	push();
+	translate(-cameraPosX, 0);
 
 	// Draw collectable item
-	if(dist(gameChar_x, gameChar_y, collectable.x_pos, collectable.y_pos) < 20)
-	{
+	if (dist(gameChar_x, gameChar_y, collectable.x_pos, collectable.y_pos) < 20) {
 		collectable.isFound = true;
 	}
-	if(collectable.isFound == false)
-	{
-	noFill();
-	stroke(255, 0, 0);
-	strokeWeight(3);
-	ellipse(collectable.x_pos, collectable.y_pos-20, collectable.size, collectable.size);
-	stroke(255, 255, 0);
-	strokeWeight(3);
-	line(collectable.x_pos-10, collectable.y_pos-20, collectable.x_pos+10, collectable.y_pos-20);
-	line(collectable.x_pos, collectable.y_pos-30, collectable.x_pos, collectable.y_pos-10);
 
-}
+	if (collectable.isFound == false) {
 
-	// Draw clouds.
-	for(var i = 0; i < clouds.length; i++)
-	{
+		fill(255, 0, 0);
+		ellipse(collectable.x_pos, collectable.y_pos-20, collectable.size, collectable.size);
+		fill(255, 255, 0);
+		ellipse(collectable.x_pos, collectable.y_pos-20, collectable.size - 10, collectable.size - 10);
+		fill(255, 0, 0);
+		ellipse(collectable.x_pos, collectable.y_pos-20, collectable.size - 20, collectable.size - 20);
+		fill(255, 255, 0);
+		ellipse(collectable.x_pos, collectable.y_pos-20, collectable.size - 30, collectable.size - 30);
+		fill(255, 0, 0);
+		ellipse(collectable.x_pos, collectable.y_pos-20, collectable.size - 40, collectable.size - 40);
+		
+	}
+
+	// Draw clouds
+	for (var i = 0; i < clouds.length; i++) {
 		stroke(255);
 		fill(255);
 		ellipse(clouds[i].x_pos, clouds[i].y_pos, 100, 100);
@@ -104,186 +105,168 @@ function draw()
 		ellipse(clouds[i].x_pos - 25, clouds[i].y_pos - 25, 100, 100);
 	}
 
-// Draw mountains.
-for(var i = 0; i < mountains.length; i++)
-{
-	stroke(196, 164, 132)
-	fill(196, 164, 132);
-	triangle(mountains[i].x_pos, floorPos_y, mountains[i].x_pos + 100, floorPos_y - 200, mountains[i].x_pos + 200, floorPos_y);
-	triangle(mountains[i].x_pos + 200, floorPos_y, mountains[i].x_pos + 300, floorPos_y - 200, mountains[i].x_pos + 400, floorPos_y);
-}
+	// Draw mountains
+	for (var i = 0; i < mountains.length; i++) {
+		stroke(196, 164, 132);
+		fill(196, 164, 132);
+		triangle(mountains[i].x_pos, floorPos_y, mountains[i].x_pos + 100, floorPos_y - 200, mountains[i].x_pos + 200, floorPos_y);
+		triangle(mountains[i].x_pos + 200, floorPos_y, mountains[i].x_pos + 300, floorPos_y - 200, mountains[i].x_pos + 400, floorPos_y);
+	}
 
-
-	//draw the canyon
+	//Draw the canyon
 	stroke(102, 51, 0);
 	fill(102, 51, 0);
-	rect(canyon.x_pos+200, floorPos_y, canyon.width, height - floorPos_y);
+	rect(canyon.x_pos + 200, floorPos_y, canyon.width, height - floorPos_y);
 
-if(gameChar_x > canyon.x_pos+200 && gameChar_x < canyon.x_pos+200+canyon.width && gameChar_y >= floorPos_y)
-{
-	isPlummeting = true;
-}
+	if (gameChar_x > canyon.x_pos + 200 && gameChar_x < canyon.x_pos + 200 + canyon.width && gameChar_y >= floorPos_y) {
+		isPlummeting = true;
+	}
 
-if(isPlummeting == true)
-{
-	gameChar_y += 5;
-}
+	if (isPlummeting == true) {
+		gameChar_y += 5;
+	}
 
-
-	//draw the trees
-	for(var i = 0; i < trees_x.length; i++)
-	{
+	//Draw the trees
+	for (var i = 0; i < trees_x.length; i++) {
 		fill(0, 155, 0);
 		ellipse(trees_x[i], floorPos_y - 50, 50, 100);
 		fill(255, 0, 0);
 		ellipse(trees_x[i], floorPos_y - 100, 100, 100);
-
-
 	}
-
 
 	stroke(0);
 	//the game character
-	if(isLeft && isFalling)
-	{
+	if (isLeft && isFalling) {
 		// add your jumping-left code
-
 		fill(255);
 		rect(gameChar_x - 15, gameChar_y - 55, 30, 50);
-		fill(0)
+		fill(0);
 		ellipse(gameChar_x, gameChar_y - 55, 40, 40);
-		fill(0)
+		fill(0);
 		rect(gameChar_x - 16, gameChar_y - 10, 10, 10);
 		rect(gameChar_x + 6, gameChar_y - 10, 10, 10);
-		fill(255)
-		ellipse(gameChar_x - 10, gameChar_y - 55, 10, 10);
-		ellipse(gameChar_x + 10, gameChar_y - 55, 10, 10);
-		fill(255)
-		rect(gameChar_x - 15, gameChar_y - 5, 30, 5);
-		fill(255, 200, 0); // Yellow color for the hand
-		rect(gameChar_x -20, gameChar_y - 39, 10, 20)
-
-
-	}
-	else if(isRight && isFalling)
-	{
-		// add your jumping-right code
-
 		fill(255);
-		rect(gameChar_x - 15, gameChar_y - 55, 30, 50); 
-		fill(0)
-		ellipse(gameChar_x, gameChar_y - 55, 40, 40);
-		fill(0)
-		rect(gameChar_x - 16, gameChar_y - 10, 10, 10);
-		rect(gameChar_x + 6, gameChar_y - 10, 10, 10);
-		fill(255)
 		ellipse(gameChar_x - 10, gameChar_y - 55, 10, 10);
 		ellipse(gameChar_x + 10, gameChar_y - 55, 10, 10);
-		fill(255)
+		fill(255);
 		rect(gameChar_x - 15, gameChar_y - 5, 30, 5);
-		fill(255, 200, 0); // Yellow color for the hand
-		rect(gameChar_x +12, gameChar_y - 39, 10, 20)
-
-	}
-	else if(isLeft)
-	{
+		fill(255, 200, 0); 
+		rect(gameChar_x - 20, gameChar_y - 39, 10, 20);
+	} else if (isRight && isFalling) {
+		// add your jumping-right code
+		fill(255);
+		rect(gameChar_x - 15, gameChar_y - 55, 30, 50);
+		fill(0);
+		ellipse(gameChar_x, gameChar_y - 55, 40, 40);
+		fill(0);
+		rect(gameChar_x - 16, gameChar_y - 10, 10, 10);
+		rect(gameChar_x + 6, gameChar_y - 10, 10, 10);
+		fill(255);
+		ellipse(gameChar_x - 10, gameChar_y - 55, 10, 10);
+		ellipse(gameChar_x + 10, gameChar_y - 55, 10, 10);
+		fill(255);
+		rect(gameChar_x - 15, gameChar_y - 5, 30, 5);
+		fill(255, 200, 0);
+		rect(gameChar_x + 12, gameChar_y - 39, 10, 20);
+	} else if (isLeft) {
 		// add your walking left code
 		fill(255);
 		rect(gameChar_x - 15, gameChar_y - 55, 30, 50);
-		fill(0)
+		fill(0);
 		ellipse(gameChar_x, gameChar_y - 55, 40, 40);
-		fill(0)
+		fill(0);
 		rect(gameChar_x - 16, gameChar_y - 10, 10, 10);
 		rect(gameChar_x + 6, gameChar_y - 10, 10, 10);
-		fill(255)
+		fill(255);
 		ellipse(gameChar_x - 10, gameChar_y - 55, 10, 10);
 		ellipse(gameChar_x + 10, gameChar_y - 55, 10, 10);
-		fill(255)
+		fill(255);
 		rect(gameChar_x - 15, gameChar_y - 5, 30, 5);
-		fill(255, 200, 0); // Yellow color for the hand
-		rect(gameChar_x -20, gameChar_y - 39, 10, 20)
-
-	}
-	else if(isRight)
-	{
+		fill(255, 200, 0);
+		rect(gameChar_x - 20, gameChar_y - 39, 10, 20);
+	} else if (isRight) {
 		// add your walking right code
-
 		fill(255);
-		rect(gameChar_x - 15, gameChar_y - 55, 30, 50); 
-		fill(0)
+		rect(gameChar_x - 15, gameChar_y - 55, 30, 50);
+		fill(0);
 		ellipse(gameChar_x, gameChar_y - 55, 40, 40);
-		fill(0)
+		fill(0);
 		rect(gameChar_x - 16, gameChar_y - 10, 10, 10);
 		rect(gameChar_x + 6, gameChar_y - 10, 10, 10);
-		fill(255)
+		fill(255);
 		ellipse(gameChar_x - 10, gameChar_y - 55, 10, 10);
 		ellipse(gameChar_x + 10, gameChar_y - 55, 10, 10);
-		fill(255)
-		rect(gameChar_x - 15, gameChar_y - 5, 30, 5);
-		fill(255, 200, 0); // Yellow color for the hand
-		rect(gameChar_x +12, gameChar_y - 39, 10, 20)
-
-	}
-	else if(isFalling || isPlummeting)
-	{
-		// add your jumping facing forwards code
-
-	}
-	else
-	{
-		// add your standing front facing code
-
 		fill(255);
-	rect(gameChar_x - 15, gameChar_y - 55, 30, 50); 
-	fill(0)
-	ellipse(gameChar_x, gameChar_y - 55, 40, 40);
-	fill(0)
-	rect(gameChar_x - 16, gameChar_y - 10, 10, 10);
-	rect(gameChar_x + 6, gameChar_y - 10, 10, 10);
-	fill(255)
-	ellipse(gameChar_x - 10, gameChar_y - 55, 10, 10);
-	ellipse(gameChar_x + 10, gameChar_y - 55, 10, 10);
-	fill(255)
-	rect(gameChar_x - 15, gameChar_y - 5, 30, 5);
+		rect(gameChar_x - 15, gameChar_y - 5, 30, 5);
+		fill(255, 200, 0); 
+		rect(gameChar_x + 12, gameChar_y - 39, 10, 20);
+	} else if (isFalling || isPlummeting) {
+		// add your jumping facing forwards code
+		fill(255);
+		rect(gameChar_x - 15, gameChar_y - 55, 30, 50);
+		fill(0);
+		ellipse(gameChar_x, gameChar_y - 55, 40, 40);
+		fill(0);
+		rect(gameChar_x - 16, gameChar_y - 10, 10, 10);
 
+		rect(gameChar_x + 6, gameChar_y - 10, 10, 10);
+		fill(255);
+		ellipse(gameChar_x - 10, gameChar_y - 55, 10, 10);
+		ellipse(gameChar_x + 10, gameChar_y - 55, 10, 10);
+		fill(255);
+		rect(gameChar_x - 15, gameChar_y - 5, 30, 5);
+		fill(255, 200, 0);
+		rect(gameChar_x - 20, gameChar_y - 39, 10, 20);
+		rect(gameChar_x + 12, gameChar_y - 39, 10, 20);
+
+	} else {
+		// add your standing front facing code
+		fill(255);
+		rect(gameChar_x - 15, gameChar_y - 55, 30, 50);
+		fill(0);
+		ellipse(gameChar_x, gameChar_y - 55, 40, 40);
+		fill(0);
+		rect(gameChar_x - 16, gameChar_y - 10, 10, 10);
+		rect(gameChar_x + 6, gameChar_y - 10, 10, 10);
+		fill(255);
+		ellipse(gameChar_x - 10, gameChar_y - 55, 10, 10);
+		ellipse(gameChar_x + 10, gameChar_y - 55, 10, 10);
+		fill(255);
+		rect(gameChar_x - 15, gameChar_y - 5, 30, 5);
 	}
+
+	pop(); 
 
 	///////////INTERACTION CODE//////////
 	//Put conditional statements to move the game character below here
 
-	if(isLeft)
-	{
+	if (isLeft) {
 		gameChar_x -= 5;
 	}
-	if(isRight)
-	{
+	if (isRight) {
 		gameChar_x += 5;
 	}
 
+	cameraPosX = gameChar_x - width / 2;
+
 	//gravity
-	if(gameChar_y < floorPos_y)
-	{
+	if (gameChar_y < floorPos_y) {
 		gameChar_y += 2;
-		isFalling = true;	
-}else{
-	isFalling = false;
-
+		isFalling = true;
+	} else {
+		isFalling = false;
+	}
 }
-}
 
-
-function keyPressed()
-{
+function keyPressed() {
 	// if statements to control the animation of the character when
 	// keys are pressed.
 
-	if(keyCode == 37)
-	{
+	if (keyCode == 65) {
 		isLeft = true;
-	} else if(keyCode == 39)
-	{
+	} else if (keyCode == 68) {
 		isRight = true;
-	} else if (keyCode == 87){
+	} else if (keyCode == 87) {
 		gameChar_y -= 100;
 	}
 
@@ -292,16 +275,13 @@ function keyPressed()
 	console.log("keyPressed: " + keyCode);
 }
 
-function keyReleased()
-{
+function keyReleased() {
 	// if statements to control the animation of the character when
 	// keys are released.
 
-	if(keyCode == 37)
-	{
+	if (keyCode == 65) {
 		isLeft = false;
-	}else if(keyCode == 39)
-	{
+	} else if (keyCode == 68) {
 		isRight = false;
 	}
 	console.log("keyReleased: " + key);
