eaddba66bfd1a0c7f683759db3f507de80736e83
Ruth Goeller <ruthgoeller@Ruths-Air.lan>
2023-08-15 10:38:03 -0500

    update

--- sketch.js
+++ sketch.js
@@ -19,25 +19,12 @@ var flagpole = {
 };
 var lives;
 
-
-
 // Initialize the game
 function setup() {
 	createCanvas(1024, 576);
 	floorPos_y = height * 3/4;
-	lives = 3; // Reset the lives
+	lives = 3;
 	startGame(); 
-	
-}
-
-
-// Initialize the collectable
-function initCollectable() {
-	collectable = [
-		{x_pos: 100, y_pos: floorPos_y, size: 40, isFound: false},
-		{x_pos: 700, y_pos: floorPos_y, size: 40, isFound: false}
-	];
-	
 }
 
 // Initialize the environment
@@ -53,15 +40,68 @@ function initEnvironment() {
 		{x_pos: 300, y_pos: 100},
 		{x_pos: 500, y_pos: 100}
 	];
+	collectable = [
+		{x_pos: 100, y_pos: floorPos_y, size: 40, isFound: false},
+		{x_pos: 700, y_pos: floorPos_y, size: 40, isFound: false}
+	];
 	canyon = 
 	[
 		{x_pos: 200, width: 100},
 		{x_pos: 700, width: 100},
-		
-
-	];
-	
+	];	
 }
+
+
+// Draw the game
+function draw() {
+	background(100, 155, 255); 
+	drawBackground();
+	checkFlagpole();
+	
+	// Draw game elements
+	checkPlayerDie()
+	moveCamera()
+    drawLifeTokens()
+	drawClouds()
+	drawMountains()
+	drawCanyon()
+	drawTrees()
+	drawCollectable() 
+	drawGameChar()
+	displayScore();
+	cameraPosX = gameChar_x - width / 2;
+	renderFlagpole();
+	pop(); 
+
+	// Logic to make the game character move or the background scroll.
+	if (lives < 1) {
+        displayGameOver("Game over. Press space to continue.");;
+        return; 
+    }else if (flagpole.isReached) {
+		displayGameOver("Level complete. Press space to continue.");
+		return;
+	}else{
+		displayScore();
+	}
+	
+	//Put conditional statements to move the game character below here
+	if (isLeft) {
+		gameChar_x -= 5;
+	}
+	if (isRight) {
+		gameChar_x += 5;
+	}
+
+	// Logic to make the game character rise and fall.
+	if (gameChar_y < floorPos_y) {
+		gameChar_y += 2;
+		isFalling = true;
+	} else {
+		isFalling = false;
+	}
+
+}
+
 
 // Draw the game background
 function drawBackground() {
@@ -69,27 +109,23 @@ function drawBackground() {
 	fill(0, 155, 0);
 	rect(0, floorPos_y, width, height - floorPos_y);
 }
-function draw() {
-	checkPlayerDie()
-	background(100, 155, 255); //fill the sky blue
 
-	noStroke();
-	fill(0, 155, 0);
-	rect(0, floorPos_y, width, height - floorPos_y); //draw some green ground
-
-	// Move the camera
+// Move the camera
+function moveCamera() {
 	push();
 	translate(-cameraPosX, 0);
+}
 
-//draw life tokens
+// Draw life tokens
+function drawLifeTokens() {
 	for (var i = 0; i < lives; i++) {
         fill(255, 0, 0);
         ellipse(30 + i * 30, 40, 20, 20); // Adjust the positions and sizes as needed
-   
 	}
-	
-	
-	// Draw clouds
+}
+
+// Draw clouds
+function drawClouds() {
 	for (var i = 0; i < clouds.length; i++) {
 		stroke(255);
 		fill(255);
@@ -100,7 +136,11 @@ function draw() {
 		ellipse(clouds[i].x_pos - 25, clouds[i].y_pos + 25, 100, 100);
 		ellipse(clouds[i].x_pos - 25, clouds[i].y_pos - 25, 100, 100);
 	}
+}
 
+
+// Draw mountains
+function drawMountains() {
 	// Draw mountains
 	for (var i = 0; i < mountains.length; i++) {
 		stroke(196, 164, 132);
@@ -108,58 +148,58 @@ function draw() {
 		triangle(mountains[i].x_pos, floorPos_y, mountains[i].x_pos + 100, floorPos_y - 200, mountains[i].x_pos + 200, floorPos_y);
 		triangle(mountains[i].x_pos + 200, floorPos_y, mountains[i].x_pos + 300, floorPos_y - 200, mountains[i].x_pos + 400, floorPos_y);
 	}
+}
 
-	//Draw the canyon
-
+// Draw canyons 
+function drawCanyon() {
 	for(var i = 0; i < canyon.length; i++){
 		fill(102, 51, 0);
 		rect(canyon[i].x_pos, floorPos_y, canyon[i].width, height - floorPos_y);
-
 		if(gameChar_x > canyon[i].x_pos && gameChar_x < canyon[i].x_pos + canyon[i].width && gameChar_y >= floorPos_y){
 			isPlummeting = true;
 		}
-
 		if(isPlummeting == true){
 			gameChar_y += 5;
 		}
-
-	}
-
-		// Draw collectable item
-for(var i = 0; i < collectable.length; i++){
-
-	if (!collectable[i].isFound && dist(gameChar_x, gameChar_y, collectable[i].x_pos, collectable[i].y_pos) < 20) {
-		collectable[i].isFound = true;
-		game_score += 1;
-	}
-	
-	if(collectable[i].isFound == false){
-		fill(255, 0, 0);
-			ellipse(collectable[i].x_pos, collectable[i].y_pos-20, collectable[i].size, collectable[i].size);
-			fill(255, 255, 0);
-			ellipse(collectable[i].x_pos, collectable[i].y_pos-20, collectable[i].size - 10, collectable[i].size - 10);
-			fill(255, 0, 0);
-			ellipse(collectable[i].x_pos, collectable[i].y_pos-20, collectable[i].size - 20, collectable[i].size - 20);
-			fill(255, 255, 0);
-			ellipse(collectable[i].x_pos, collectable[i].y_pos-20, collectable[i].size - 30, collectable[i].size - 30);
-			fill(255, 0, 0);
-			ellipse(collectable[i].x_pos, collectable[i].y_pos-20, collectable[i].size - 40, collectable[i].size - 40);
 	}
 }
 
-
-	checkFlagpole();
-
-	//Draw the trees
+// Draw trees
+function drawTrees() {
 	for (var i = 0; i < trees_x.length; i++) {
 		fill(0, 155, 0);
 		ellipse(trees_x[i], floorPos_y - 50, 50, 100);
 		fill(255, 0, 0);
 		ellipse(trees_x[i], floorPos_y - 100, 100, 100);
 	}
+}
 
+// Draw collectable items
+function drawCollectable() {
+	for(var i = 0; i < collectable.length; i++){
+		if (!collectable[i].isFound && dist(gameChar_x, gameChar_y, collectable[i].x_pos, collectable[i].y_pos) < 20) {
+			collectable[i].isFound = true;
+			game_score += 1;
+		}
+		
+		if(collectable[i].isFound == false){
+			fill(255, 0, 0);
+				ellipse(collectable[i].x_pos, collectable[i].y_pos-20, collectable[i].size, collectable[i].size);
+				fill(255, 255, 0);
+				ellipse(collectable[i].x_pos, collectable[i].y_pos-20, collectable[i].size - 10, collectable[i].size - 10);
+				fill(255, 0, 0);
+				ellipse(collectable[i].x_pos, collectable[i].y_pos-20, collectable[i].size - 20, collectable[i].size - 20);
+				fill(255, 255, 0);
+				ellipse(collectable[i].x_pos, collectable[i].y_pos-20, collectable[i].size - 30, collectable[i].size - 30);
+				fill(255, 0, 0);
+				ellipse(collectable[i].x_pos, collectable[i].y_pos-20, collectable[i].size - 40, collectable[i].size - 40);
+		}
+	}
+}
+
+// Draw game character
+function drawGameChar() {
 	stroke(0);
-	//the game character
 	if (isLeft && isFalling) {
 		// add your jumping-left code
 		fill(255);
@@ -232,7 +272,6 @@ for(var i = 0; i < collectable.length; i++){
 		ellipse(gameChar_x, gameChar_y - 55, 40, 40);
 		fill(0);
 		rect(gameChar_x - 16, gameChar_y - 10, 10, 10);
-
 		rect(gameChar_x + 6, gameChar_y - 10, 10, 10);
 		fill(255);
 		ellipse(gameChar_x - 10, gameChar_y - 55, 10, 10);
@@ -258,54 +297,24 @@ for(var i = 0; i < collectable.length; i++){
 		fill(255);
 		rect(gameChar_x - 15, gameChar_y - 5, 30, 5);
 	}
+}
 
 
-	if (lives < 1) {
-        fill(255);
-        textSize(20);
-        text("Game over. Press space to continue.", width / 2, height / 2);
-        return; 
-    }
+//display gameover 
+function displayGameOver(message) {
+    fill(255);
+    textSize(20);
+    text(message, width / 2, height / 2);
+}
 
-    // Check for level complete
-    if (flagpole.isReached) {
-        fill(255);
-        textSize(20);
-        text("Level complete. Press space to continue.", width / 2, height / 2);
-        return; 
-    }
-
-	renderFlagpole();
-	pop(); 
-
-	fill(255);
-	noStroke();
-	text("Score: " + game_score, 20, 20);
-	//Put conditional statements to move the game character below here
-
-	if (isLeft) {
-		gameChar_x -= 5;
-	}
-	if (isRight) {
-		gameChar_x += 5;
-	}
-
-	cameraPosX = gameChar_x - width / 2;
-
-	//gravityasaaa
-	if (gameChar_y < floorPos_y) {
-		gameChar_y += 2;
-		isFalling = true;
-	} else {
-		isFalling = false;
-	}
-
+// Display Score 
+function displayScore() {
+    fill(255);
+    noStroke();
+    text("Score: " + game_score, 20, 20);
 }
 
 function keyPressed() {
-	// if statements to control the animation of the character when
-	// keys are pressed.
-
 	if (keyCode == 65) {
 		isLeft = true;
 	} else if (keyCode == 68) {
@@ -316,9 +325,6 @@ function keyPressed() {
 }
 
 function keyReleased() {
-	// if statements to control the animation of the character when
-	// keys are released.
-
 	if (keyCode == 65) {
 		isLeft = false;
 	} else if (keyCode == 68) {
@@ -335,7 +341,6 @@ function renderFlagpole() {
 	fill(255, 0, 255);
 	noStroke();
 
-
     if (flagpole.isReached) {
         // Draw flagpole when it's reached
 		rect(flagpole.x_pos, floorPos_y - 250, 50, 50);
@@ -346,32 +351,20 @@ function renderFlagpole() {
 }
 
 function checkFlagpole() {
-
 	var d = abs(gameChar_x - flagpole.x_pos);
-
-	console.log(d);
-
    if(d < 15) {
 	   flagpole.isReached = true;
    }
 }
 
 
-
 function checkPlayerDie() {
-
-
-
 	if(gameChar_y > height){
 		lives -= 1;
-
 		if(lives > 0){
 			startGame();
 		}
 	}
-
-
-
 }
 
 // Start the game
@@ -382,7 +375,6 @@ function startGame() {
 	isRight = false;
 	isFalling = false;
 	isPlummeting = false;
-	initCollectable();
 	initEnvironment();
 	
 }
